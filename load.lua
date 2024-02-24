18f3891c7d1c5cde0dc87754b119d9b135f39935
xz <95953670+laagginq@users.noreply.github.com>
2024-02-24 13:26:23 -0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,39 +1,37 @@
 -- hvh.lua
 
-
 if _G.hvhLoaded then 
-    return
+	return
 else
-    _G.hvhLoaded = true
+	_G.hvhLoaded = true
 end
 
 if not isfolder("hvh") then 
-    makefolder("hvh")
+	makefolder("hvh")
 end
 
 if not isfile("hvh/key.txt") then
-    writefile("hvh/key.txt","")
+	writefile("hvh/key.txt","")
 end
 
-
-local hvhlua_loader = Instance.new("ScreenGui")
+local Login = Instance.new("ScreenGui")
 local Holder = Instance.new("Frame")
 local Title = Instance.new("TextLabel")
-local KeyboxBack = Instance.new("Frame")
-local KeyBox = Instance.new("TextBox")
-local Button = Instance.new("TextButton")
+local Close = Instance.new("TextButton")
+local KeyInput = Instance.new("Frame")
+local Input = Instance.new("TextBox")
+local Login_2 = Instance.new("TextButton")
 
-hvhlua_loader.Name = "hvh.lua_loader"
-hvhlua_loader.Parent = game.CoreGui
-hvhlua_loader.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Login.Name = "Login"
+Login.Parent = game.CoreGui
+Login.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Holder.Name = "Holder"
-Holder.Parent = hvhlua_loader
-Holder.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
-Holder.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Holder.BorderSizePixel = 0
-Holder.Position = UDim2.new(0.37500006, 0, 0.456757218, 0)
-Holder.Size = UDim2.new(0.248437494, 0, 0.139560789, 0)
+Holder.Parent = Login
+Holder.BackgroundColor3 = Color3.fromRGB(10, 10, 10)
+Holder.BorderColor3 = Color3.fromRGB(111, 50, 168)
+Holder.Position = UDim2.new(0.344531238, 0, 0.346367627, 0)
+Holder.Size = UDim2.new(0, 366, 0, 118)
 
 Title.Name = "Title"
 Title.Parent = Holder
@@ -41,53 +39,63 @@ Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Title.BackgroundTransparency = 1.000
 Title.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Title.BorderSizePixel = 0
-Title.Size = UDim2.new(1, 0, 0.298970908, 0)
-Title.Font = Enum.Font.Unknown
-Title.Text = "hvh.lua login"
+Title.Size = UDim2.new(0, 367, 0, 27)
+Title.Font = Enum.Font.Code
+Title.Text = " hvh.lua | Login"
 Title.TextColor3 = Color3.fromRGB(255, 255, 255)
-Title.TextSize = 18.000
+Title.TextSize = 14.000
+Title.TextXAlignment = Enum.TextXAlignment.Left
 
-KeyboxBack.Name = "KeyboxBack"
-KeyboxBack.Parent = Holder
-KeyboxBack.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-KeyboxBack.BorderColor3 = Color3.fromRGB(0, 0, 0)
-KeyboxBack.BorderSizePixel = 0
-KeyboxBack.Position = UDim2.new(0.034591388, 0, 0.291545302, 0)
-KeyboxBack.Size = UDim2.new(0.933962286, 0, 0.209279642, 0)
+Close.Name = "Close"
+Close.Parent = Holder
+Close.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Close.BackgroundTransparency = 1.000
+Close.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Close.BorderSizePixel = 0
+Close.Position = UDim2.new(0.925545454, 0, 0, 0)
+Close.Size = UDim2.new(0, 27, 0, 27)
+Close.Font = Enum.Font.Ubuntu
+Close.Text = "X"
+Close.TextColor3 = Color3.fromRGB(255, 255, 255)
+Close.TextSize = 20.000
 
-KeyBox.Name = "KeyBox"
-KeyBox.Parent = KeyboxBack
-KeyBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-KeyBox.BackgroundTransparency = 1.000
-KeyBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
-KeyBox.BorderSizePixel = 0
-KeyBox.Position = UDim2.new(0.0235690232, 0, 0, 0)
-KeyBox.Size = UDim2.new(0.976430953, 0, 1, 0)
-KeyBox.Font = Enum.Font.Ubuntu
-KeyBox.PlaceholderText = "Enter your key here..."
-KeyBox.Text = readfile("hvh/key.txt")
-KeyBox.TextColor3 = Color3.fromRGB(255, 255, 255)
-KeyBox.TextSize = 14.000
-KeyBox.TextXAlignment = Enum.TextXAlignment.Left
+KeyInput.Name = "KeyInput"
+KeyInput.Parent = Holder
+KeyInput.BackgroundColor3 = Color3.fromRGB(10, 10, 10)
+KeyInput.BorderColor3 = Color3.fromRGB(111, 50, 168)
+KeyInput.Position = UDim2.new(0.0983606577, 0, 0.246289268, 0)
+KeyInput.Size = UDim2.new(0, 294, 0, 23)
 
-Button.Name = "Button"
-Button.Parent = Holder
-Button.Active = false
-Button.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-Button.BorderColor3 = Color3.fromRGB(0, 0, 0)
-Button.BorderSizePixel = 0
-Button.Position = UDim2.new(0.351067126, 0, 0.647561371, 0)
-Button.Selectable = false
-Button.Size = UDim2.new(0.298742145, 0, 0.209279642, 0)
-Button.Font = Enum.Font.Ubuntu
-Button.Text = "Login"
-Button.TextColor3 = Color3.fromRGB(255, 255, 255)
-Button.TextSize = 14.000
+Input.Name = "Input"
+Input.Parent = KeyInput
+Input.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Input.BackgroundTransparency = 1.000
+Input.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Input.BorderSizePixel = 0
+Input.Position = UDim2.new(0.0238095243, 0, 0, 0)
+Input.Size = UDim2.new(0, 287, 0, 23)
+Input.Font = Enum.Font.Code
+Input.PlaceholderText = "Enter key here..."
+Input.Text = readfile("hvh/key.txt")
+Input.TextColor3 = Color3.fromRGB(255, 255, 255)
+Input.TextSize = 14.000
+Input.TextXAlignment = Enum.TextXAlignment.Left
 
-Button.MouseButton1Click:Connect(function()
-    hvhlua_loader:Destroy()
-    local key = tostring(KeyBox.Text)
-    writefile("hvh/key.txt",key)
-    script_key = key;
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/hvh/main/src.lua"))()
+Login_2.Name = "Login"
+Login_2.Parent = Holder
+Login_2.BackgroundColor3 = Color3.fromRGB(10, 10, 10)
+Login_2.BorderColor3 = Color3.fromRGB(111, 50, 168)
+Login_2.Position = UDim2.new(0.0983606577, 0, 0.542372882, 0)
+Login_2.Size = UDim2.new(0, 294, 0, 23)
+Login_2.Font = Enum.Font.Code
+Login_2.Text = "Login"
+Login_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Login_2.TextSize = 14.000
+
+Login_2.MouseButton1Click:Connect(function()
+	Login:Destroy()
+	local key = tostring(Input.Text)
+	writefile("hvh/key.txt",key)
+	script_key = key;
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/laagginq/hvh/main/src.lua"))()
 end)
